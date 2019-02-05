f56ea6e66e8c277a1bfb84ca1640daf03eeb024e
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2019-02-05 16:41:32 +0200

    update components

--- vue-form.js
+++ vue-form.js
@@ -14,6 +14,7 @@ Vue.component('vue-form-group', {
 
 var VueFormInput = Vue.component('vue-form-input', {
     props: {
+        size:  {type: String},
         name:  {type: String},
         type:  {type: String, default: 'text'},
         value: {type: [String, Number]},
@@ -24,7 +25,46 @@ var VueFormInput = Vue.component('vue-form-input', {
             set: function (val) { this.$emit('input', val); }
         }
     },
-    template: '<input ref="field" class="form-control" :id="_uid" :name="name" :type="type" v-model="_value">'
+    template: '<input ref="field" :class="\'form-control\' + (size ? \' input-\' + size : \'\')" :id="_uid" :name="name" :type="type" v-model="_value">'
+});
+
+Vue.component('vue-form-number', {
+    props: {
+        size:  {type: String},
+        name:  {type: String},
+        value: {type: [String, Number], default: 0},
+        step:  {type: Number, default: 1, validator: function (value) { return value > 0; }},
+        min:   {type: Number, default: null},
+        max:   {type: Number, default: null}
+    },
+    computed: {
+        _value: {
+            get: function () { return this.value; },
+            set: function (val) { this.$emit('input', val); }
+        }
+    },
+    template:
+        '<div :class="\'input-group \' + (size ? \'input-group-\' + size : \'\')">' +
+        '    <span class="input-group-btn">' +
+        '        <button :disabled="null !== min && min >= _value" :class="\'btn btn-default \' + (size ? \'btn-\' + size : \'\')" type="button" v-on:click="decrement()">' +
+        '            <i class="fa fa-fw fa-minus"></i>' +
+        '        </button>' +
+        '    </span>' +
+        '    <input type="text" ref="field" class="form-control" :id="_uid" :name="name" :min="min" :max="max" v-model="_value">' +
+        '    <span class="input-group-btn">' +
+        '        <button :disabled="null !== max && max <= _value" :class="\'btn btn-default \' + (size ? \'btn-\' + size : \'\')" type="button" v-on:click="increment()">' +
+        '            <i class="fa fa-fw fa-plus"></i>' +
+        '        </button>' +
+        '    </span>' +
+        '</div>',
+    methods: {
+        decrement: function () {
+            this._value -= this.step;
+        },
+        increment: function () {
+            this._value += this.step;
+        }
+    }
 });
 
 Vue.component('vue-form-textarea', {
@@ -87,15 +127,24 @@ Vue.component('vue-form-radio', {
 
 Vue.component('vue-form-choice', {
     props: {
-        name:     {type: String},
-        value:    {type: [Boolean, String, Number, Array]},
-        choices:  {type: Object},
-        expanded: {type: Boolean},
-        multiple: {type: Boolean},
-        nullable: {type: Boolean},
-        inline:   {type: Boolean, default: false}
+        size:        {type: String},
+        name:        {type: String},
+        value:       {type: [Boolean, String, Number, Array]},
+        placeholder: {type: String},
+        choices:     {type: Object},
+        expanded:    {type: Boolean},
+        multiple:    {type: Boolean},
+        nullable:    {type: Boolean},
+        inline:      {type: Boolean, default: false}
     },
     computed: {
+        _classes: function () {
+            var classes = ['form-control'];
+
+            if (this.size) { classes.push('input-' + this.size) }
+
+            return classes.join(' ');
+        },
         _choices: function () {
             var _choices = [];
 
@@ -107,7 +156,7 @@ Vue.component('vue-form-choice', {
         }
     },
     template:
-        '<select ref="field" v-if="!expanded" class="form-control" :name="name" :multiple="multiple">' +
+        '<select ref="field" v-if="!expanded" :class="_classes" :name="name" :multiple="multiple" :placeholder="placeholder" :data-placeholder="placeholder">' +
         '    <option v-for="choice in _choices" v-bind:value="choice.id">{{ choice.label }}</option>' +
         '</select>' +
         '<div v-else-if="!multiple">' +
@@ -145,6 +194,7 @@ Vue.component('vue-form-duration', {
 Vue.component('vue-form-date', {
     extends: VueFormInput,
     props: {
+        size:        {type: String},
         name:        {type: String},
         value:       {type: String},
         inline:      {type: Boolean, default: false},
@@ -152,15 +202,16 @@ Vue.component('vue-form-date', {
         formatView:  {type: String, default: 'L'},
     },
     template:
-        '<div>' +
+        '<div v-if="inline">' +
         '    <input ref="field" type="hidden" :id="_uid" :name="name" v-model="_value">' +
-        '    <div v-if="inline" ref="picker"></div>' +
-        '    <div v-else class="input-group">' +
-        '        <input ref="picker" type="text" class="form-control">' +
-        '        <span class="input-group-addon">' +
-        '            <span class="fa fa-calendar"></span>' +
-        '        </span>' +
-        '    </div>' +
+        '    <div ref="picker"></div>' +
+        '</div>' +
+        '<div v-else :class="\'input-group\' + (size ? \' input-group-\' + size : null)">' +
+        '    <input ref="field" type="hidden" :id="_uid" :name="name" v-model="_value">' +
+        '    <input ref="picker" type="text" class="form-control">' +
+        '    <span class="input-group-addon">' +
+        '        <span class="fa fa-calendar"></span>' +
+        '    </span>' +
         '</div>',
     mounted: function () {
         $(this.$refs.picker).datetimepicker({
@@ -168,7 +219,7 @@ Vue.component('vue-form-date', {
             date:   moment(this.value, this.formatValue),
             inline: this.inline
         }).on('dp.change', function (event) {
-            var time = event.date.format(this.formatValue);
+            var time = event.date ? event.date.format(this.formatValue) : null;
 
             this.$refs.field.value = time;
             this.$emit('input', time);
