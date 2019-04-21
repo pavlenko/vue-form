f6e92478f82349d0a63b8a75f671651f60469a4f
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2019-04-21 09:52:37 +0300

    add todo for validate

---
+++ v-form-input.js
@@ -0,0 +1,46 @@
+VForm.components.VFormInput = Vue.extend({
+    props: {
+        id: {
+            type: String
+        },
+        name: {
+            type: String
+        },
+        value: {
+            type:    [String, Number],
+            default: null
+        },
+        error: {
+            type:    Object,
+            default: function () { return {}; }
+        },
+        disabled: {
+            type:    Boolean,
+            default: false
+        },
+        readonly: {
+            type:    Boolean,
+            default: false
+        }
+    },
+    computed: {
+        _id: function () { return this.id || 'v-form-' + this._uid; },
+        _name: function () { return this.name || 'v-form-' + this._id; },
+        _value: {
+            get: function () { return this.value; },
+            set: function (value) { this.$emit('input', value); }
+        }
+    },
+    template: '<input :id="_id" :name="_name" v-model="_value" />',
+    mounted: function () {},
+    watch: {
+        _value: {
+            handler: function (newValue, oldValue) {
+                if (newValue !== oldValue) {
+                    //this.$emit('update:error', {required: !!String(newValue).trim().length});
+                    //this.error.required = !!String(newValue).trim().length;
+                }
+            }
+        }
+    }
+});
---
+++ v-form.js
@@ -0,0 +1,31 @@
+var VForm = {
+    directives: {},
+    components: {},
+    validators: {},
+    install: function (vue, options) {
+        Object.keys(VForm.components).forEach(function (key) {
+            vue.component(key, VForm.components[key]);
+        });
+
+        Object.keys(VForm.directives).forEach(function (key) {
+            vue.directive(key, VForm.directives[key]);
+        });
+    },
+    deepGet: function (object, path, create) {
+        var parts = path.split('.'), current = object, index = 0;
+
+        for (index; index < parts.length; ++index) {
+            if (current[parts[index]] === undefined) {
+                if (create) {
+                    //current[parts[index]]
+                } else {
+                    return undefined;
+                }
+            } else {
+                current = current[parts[index]];
+            }
+        }
+
+        return current;
+    }
+};
